d7de5d01cc4da7e6482b4bda18350cb2f3f2c886
lorisleiva <lorisleiva@users.noreply.github.com>
2020-11-15 17:17:38 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Lorisleiva\RequestController\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Lorisleiva\RequestController\RequestControllerServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
