09b4495779a1458eb4707fe9e53dc6db63b0ece9
lorisleiva <lorisleiva@users.noreply.github.com>
2020-11-15 17:26:49 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Lorisleiva\RequestController\Tests;
 
-use Lorisleiva\RequestController\RequestControllerServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
