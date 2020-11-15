bf84ef8265b6c9f54d7c18aaaa2bc657e85dd8e0
Loris Leiva <loris.leiva@gmail.com>
2020-11-15 17:26:26 +0000

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,40 +2,10 @@
 
 namespace Lorisleiva\RequestController\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Lorisleiva\RequestController\RequestControllerServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Lorisleiva\\RequestController\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [
-            RequestControllerServiceProvider::class,
-        ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('database.default', 'sqlite');
-        $app['config']->set('database.connections.sqlite', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ]);
-
-        /*
-        include_once __DIR__.'/../database/migrations/create_request_controller_table.php.stub';
-        (new \CreatePackageTable())->up();
-        */
-    }
+    //
 }
